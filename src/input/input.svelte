c3e81b32af7446d53a03e65ac60a3822ce19cf18
Miro Ostafinski <ostafinskim@gmail.com>
2025-10-28 14:28:14 +0000

    user-invalid

--- src/input/input.svelte
+++ src/input/input.svelte
@@ -7,7 +7,7 @@
 <div class="space-y-1.5">
   <label class="block font-medium text-slate-700" for={id}>{label}</label>
   <input
-    class="block rounded px-2 py-1.5 outline outline-offset-2 outline-slate-600 placeholder:italic focus:outline-2 focus:outline-blue-500"
+    class="block rounded px-2 py-1.5 outline outline-offset-2 outline-slate-600 placeholder:italic user-invalid:outline-red-500 focus:outline-2 focus:outline-blue-500"
     {id}
     aria-describedby={descriptionId}
     aria-invalid={!!error}
