93ab99de2f0dfdbc8152acf9d99aaa08b757e306
Miro Ostafinski <ostafinskim@gmail.com>
2025-10-28 14:00:01 +0000

    card

--- src/card/card.svelte
+++ src/card/card.svelte
@@ -2,7 +2,7 @@
   const { title = 'Card Title', description = '' } = $props();
 </script>
 
-<div>
-  <h2>{title}</h2>
-  <p>{description}</p>
+<div class="max-w-fit m-auto shadow rounded-lg outline-violet-600 outline border-slate-400 p-8 text-center space-y-5">
+  <h2 class="font-black underline underline-offset-7 text-4xl">{title}</h2>
+  <p class="text-2xl">{description}</p>
 </div>
