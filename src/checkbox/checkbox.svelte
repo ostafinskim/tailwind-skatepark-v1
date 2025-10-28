53aa902a2b92cb9b2306a60fd49ac001844e86b4
Miro Ostafinski <ostafinskim@gmail.com>
2025-10-28 14:31:22 +0000

    focus-within, accent

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -2,7 +2,9 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div>
-  <input {id} {...props} type="checkbox" />
+<div
+  class="rounded border bg-slate-200 p-4 outline-offset-2 outline-sky-400 focus-within:bg-sky-200"
+>
+  <input class="accent-purple-500" {id} {...props} type="checkbox" />
   <label for={id}>{label}</label>
 </div>
