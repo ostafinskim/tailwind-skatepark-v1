ae605a53fb3656e724e69079845ca7968ebf29ac
Miro Ostafinski <ostafinskim@gmail.com>
2025-10-31 15:54:29 +0000

    inner submit button

--- src/input-with-button/input-with-button.svelte
+++ src/input-with-button/input-with-button.svelte
@@ -1,5 +1,7 @@
-<form>
-  <label for="new-task">What do you need to do?</label>
-  <input type="text" id="new-task" placeholder="New task…" />
-  <button>Submit</button>
+<form
+  class="flex rounded px-3 py-1.5 ring-purple-500 outline outline-slate-600 focus-within:ring-4 has-user-invalid:ring-red-600"
+>
+  <label for="new-task" class="sr-only">Your email</label>
+  <input class="flex-1 outline-none" type="email" id="new-task" placeholder="Email" required />
+  <button class="rounded-md bg-purple-600 px-3 py-1.5 text-white">Submit</button>
 </form>
